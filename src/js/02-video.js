e9566233e652893a7bc2be1453490bfbad6920b2
Bill-Ukraine <chermetbeloff@gmail.com>
2023-06-09 00:45:32 +0300

    9.06

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_PLAYER_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlayTime, 1000));
+
+function onPlayTime(data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem(STORAGE_PLAYER_KEY, currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem(STORAGE_PLAYER_KEY) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
 
+import throttle from 'lodash.throttle';
+
+
+  formEl = document.querySelector(`.feedback-form`);
+  emailEl = document.querySelector(`.feedback-form input`);
+  messageEl = document.querySelector(`.feedback-form textarea`);
+
+
+const INPUT_KEY = 'feedback-form-state';
+const localString = localStorage.getItem(INPUT_KEY);
+const localObject = JSON.parse(localString);
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+if (localObject) {
+  emailEl.value = localObject.email;
+  messageEl.value = localObject.message;
+}
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  formData = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+
+  localStorage.setItem(INPUT_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  formData = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+
+  console.log(formData);
+  localStorage.removeItem(INPUT_KEY);
+  formEl.reset();
+};
